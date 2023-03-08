9186ec3f4e5cb1500a06242203161f4c342b80f1
haden <git@haden.io>
2023-03-08 03:31:58 -0800

    Updates new tab checking to only edge since bing chat doesn't work on chrome

--- newtab.js
+++ newtab.js
@@ -4,7 +4,7 @@ var newTabURL = "https://www.bing.com/search?q=Bing+AI&showconv=1";
 // listen for new tab creation
 chrome.tabs.onCreated.addListener(function (tab) {
     // check if the new tab is blank
-    if (tab.url === "chrome://newtab/" || tab.url === "edge://newtab/") {
+    if (tab.url === "edge://newtab/") {
         // redirect the new tab to the URL
         chrome.tabs.update(tab.id, { url: newTabURL });
     }
